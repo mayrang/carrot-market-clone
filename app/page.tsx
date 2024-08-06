8529d6e67e1ad2b1bb062c2f41fab6d7083185fd
mayrang <pkss0626@naver.com>
2024-08-07 07:05:19 +0900

    group modifier

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
 
+// group-focus-within : group에 어떤 자식중 하나라도 포커스 되어있을 때 적용되는
+
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-lime-100 h-screen p-5 dark:bg-gray-600 flex justify-center items-center  ">
       <div className="bg-white flex flex-col  gap-4 w-full shadow-lg p-5 dark:bg-gray-800 rounded-2xl max-w-screen-sm ">
         {["GeonSang", "Me", "You", "Yourself", ""].map((person, index) => (
-          <div
-            className=" flex items-center gap-4 odd:bg-slate-100 even:bg-cyan-100 border-b-2 last:border-0 p-2.5 rounded-xl"
-            key={index}
-          >
+          <div className=" flex items-center gap-4  p-2.5 rounded-xl group" key={index}>
             <div className="size-10 bg-blue-400 rounded-full" />
-            <span className="text-lg font-medium empty:w-20 empty:h-4 empty:bg-gray-300 empty:animate-pulse empty:rounded-full">
+            <span className="text-lg font-medium empty:w-20 empty:h-4 empty:bg-gray-300 empty:animate-pulse empty:rounded-full group-hover:text-blue-300">
               {person}
             </span>
 
