455424cd1f8142b480d1c6c03bf91d73115c2382
mayrang <pkss0626@naver.com>
2024-08-07 04:29:45 +0900

    animation ping

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ export default function Home() {
       <div className="bg-white flex flex-col  gap-4 w-full shadow-lg p-5 dark:bg-gray-800 rounded-2xl max-w-screen-sm ">
         {["GeonSang", "Me", "You", "Yourself"].map((person, index) => (
           <div
-            className="*:animate-pulse flex items-center gap-4 odd:bg-slate-100 even:bg-cyan-100 border-b-2 last:border-0 p-2.5 rounded-xl"
+            className=" flex items-center gap-4 odd:bg-slate-100 even:bg-cyan-100 border-b-2 last:border-0 p-2.5 rounded-xl"
             key={index}
           >
             <div className="size-10 bg-blue-400 rounded-full" />
-            {/* <span className="text-lg font-medium">{person}</span> */}
-            <div className="w-60 h-4 rounded-full bg-gray-300" />
-            {/* <div className="size-6 bg-red-500 text-white flex item-center justify-center rounded-full">
-              <span>{index + 1}</span>
-            </div> */}
-            <div className="w-40 h-4 rounded-full bg-gray-300" />
+            <span className="text-lg font-medium">{person}</span>
+
+            <div className="size-6 bg-red-500 text-white relative flex item-center justify-center rounded-full">
+              <span className="z-10">{index + 1}</span>
+              <div className="size-6 animate-ping bg-red-500  text-white absolute flex item-center justify-center rounded-full" />
+            </div>
           </div>
         ))}
       </div>
