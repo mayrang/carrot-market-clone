dd6488ddc65b28d9995e4d9106e3f6c0b14d2df1
mayrang <pkss0626@naver.com>
2024-08-09 12:28:29 +0900

    sms validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,11 +1,41 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import validator from "validator";
-import { z } from "zod";
+import { typeToFlattenedError, z } from "zod";
 
-const phoneNumberSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneNumberSchema = z
+  .string()
+  .trim()
+  .refine((phoneNubmer) => validator.isMobilePhone(phoneNubmer, "ko-KR"));
 const tokenScema = z.coerce.number().min(100000).max(999999);
 
-export const smsValidation = (prevState: any, formData: FormData) => {
-  const token = formData.get("token");
+interface PrevState {
+  token: boolean;
+}
+
+export const smsValidation = (prevState: PrevState, formData: FormData) => {
+  if (!prevState.token) {
+    const result = phoneNumberSchema.safeParse(formData.get("phoneNumber"));
+    if (!result.success) {
+      return {
+        token: false,
+        errors: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+  } else {
+    const result = tokenScema.safeParse(formData.get("token"));
+    if (!result.success) {
+      return {
+        token: true,
+        errors: result.error.flatten(),
+      };
+    } else {
+      redirect("/");
+    }
+  }
 };
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -6,8 +6,13 @@ import React from "react";
 import { useFormState } from "react-dom";
 import { smsValidation } from "./actions";
 
+const INITIAL_STATE = {
+  token: false,
+  errors: undefined,
+};
+
 export default function SMSLogin() {
-  const [state, action] = useFormState(smsValidation, null);
+  const [state, action] = useFormState(smsValidation, INITIAL_STATE);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -15,10 +20,21 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify your phone number</h2>
       </div>
       <form action={action} className="flex flex-col gap-3">
-        <Input required placeholder="Phone number" name="phoneNumber" type="text" />
-        <Input required placeholder="Verify" name="token" type="number" min={100000} max={999999} />
+        {state.token ? (
+          <Input
+            required
+            placeholder="Verify"
+            name="token"
+            type="number"
+            min={100000}
+            max={999999}
+            errors={state.errors?.formErrors}
+          />
+        ) : (
+          <Input required placeholder="Phone number" name="phoneNumber" type="text" errors={state.errors?.formErrors} />
+        )}
 
-        <Button text="Verify" />
+        <Button text={state.token ? "Verify Token" : "Send Validation SMS"} />
       </form>
     </div>
   );
