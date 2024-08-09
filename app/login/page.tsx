03a14be89c50a157d3a6f0ccb81dc66356e91364
mayrang <pkss0626@naver.com>
2024-08-10 01:32:29 +0900

    prisma 간단하게 알아보기

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -27,7 +27,7 @@ export default function Login() {
   const [state, action] = useFormState(login, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
-      <div className="flex flex-col gap-2 *:font-medium">
+      <div className="flex flex-col gap-2 *:font-medium ">
         <h1 className="text-2xl ">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password</h2>
       </div>
