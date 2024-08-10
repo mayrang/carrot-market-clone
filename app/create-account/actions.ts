e7a858f48497684ed5e2c735f3ea11b0486740ba
mayrang <pkss0626@naver.com>
2024-08-11 01:29:13 +0900

    validation email, username by database

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,11 +1,36 @@
 "use server";
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
 
 const usernameCheck = (username: string) => username !== "May";
 
 const confirmPasswordCheck = ({ password, confirmPassword }: { password: string; confirmPassword: string }) =>
   password === confirmPassword;
+
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
 
 const formSchema = z
   .object({
@@ -18,8 +43,14 @@ const formSchema = z
       .max(15, "Username은 3글자 이하이여야 합니다.")
       .trim()
       .transform((username) => `🥕${username}`)
-      .refine(usernameCheck, "May는 사용할 수 없습니다."),
-    email: z.string().email("이메일 형식이여야 합니다.").toLowerCase(),
+      .refine(usernameCheck, "May는 사용할 수 없습니다.")
+      .refine(checkUniqueUsername, "유저 이름이 이미 사용중입니다."),
+
+    email: z
+      .string()
+      .email("이메일 형식이여야 합니다.")
+      .toLowerCase()
+      .refine(checkUniqueEmail, "이메일이 이미 사용중입니다."),
     password: z
       .string()
       .min(PASSWORD_MIN_LENGTH, `비밀번호는 ${PASSWORD_MIN_LENGTH}글자 이상이여야 합니다.`)
@@ -40,7 +71,7 @@ export const createAccount = async (prevState: any, formData: FormData) => {
     password: formData.get("password"),
     confirmPassword: formData.get("confirmPassword"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
